58539f23b8087c8c3f479f293bdee00d4535581e
Maurício <mauriomena@hotmail.com>
2024-07-04 23:22:04 -0300

    feat: Flash Point modelling, model adjusts and update of requirements.txt

--- src/model_training.py
+++ src/model_training.py
@@ -32,14 +32,25 @@ class NeuralNet:
         self.scaled_X_train = self.scaler.transform(self.X_train)
         self.scaled_X_test = self.scaler.transform(self.X_test)
 
-    def __build_model(self) -> None:
+    def __build_model(self) -> models.Model:
         model = keras.Sequential(
             [
                 layers.Dense(
-                    20, activation="relu", input_shape=[self.scaled_X_train.shape[1]]
+                    20,
+                    activation="relu",
+                    input_shape=[self.scaled_X_train.shape[1]],
+                    kernel_regularizer=keras.regularizers.L1L2(l1=0.001, l2=0.001),
                 ),
-                layers.Dense(40, activation="relu"),
-                layers.Dense(10, activation="relu"),
+                layers.Dense(
+                    40,
+                    activation="relu",
+                    kernel_regularizer=keras.regularizers.L1L2(l1=0.001, l2=0.001),
+                ),
+                layers.Dense(
+                    10,
+                    activation="relu",
+                    kernel_regularizer=keras.regularizers.L1L2(l1=0.001, l2=0.001),
+                ),
                 layers.Dense(1),
             ]
         )
@@ -51,12 +62,12 @@ class NeuralNet:
         self.nn_model = self.__build_model()
 
         # Add an early stopping
-        stop_early = keras.callbacks.EarlyStopping(monitor="val_mse", patience=200)
+        stop_early = keras.callbacks.EarlyStopping(monitor="val_mse", patience=300)
         self.nn_model.fit(
             self.scaled_X_train,
             self.y_train,
             batch_size=self.scaled_X_train.shape[0],
-            epochs=2000,
+            epochs=3000,
             validation_data=(self.scaled_X_test, self.y_test),
             callbacks=[stop_early],
             verbose=0,
@@ -65,14 +76,16 @@ class NeuralNet:
     def __model_metrics(self) -> None:
         _, self.mae, self.mse = self.nn_model.evaluate(self.scaled_X_test, self.y_test)
 
-        print(f"MAE - {self.mae}\n RMSE - {np.sqrt(self.mse)}")
+        print(f"MAE - {self.mae}\nRMSE - {np.sqrt(self.mse)}")
 
     def __save_model(self) -> None:
         # Fitted model
-        models.save_model(self.nn_model, "models/fitted_nn.keras")
+        models.save_model(
+            self.nn_model, "C:/Projetos/fp-neural-net/models/fitted_nn.keras"
+        )
 
         # Fitted scaler
-        with open("models/fitted_scaler.pkl", "wb") as f:
+        with open("C:/Projetos/fp-neural-net/models/fitted_scaler.pkl", "wb") as f:
             pkl.dump(self.scaler, f)
 
         print("Model and scaler were succesfully saved")
