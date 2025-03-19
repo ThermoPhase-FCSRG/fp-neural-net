d54a655200669fe81839b4495516c6b41ba42af3
Diego Volpatto <dtvolpatto@gmail.com>
2025-03-19 17:30:16 -0300

    Add improvements to convert numpy data to tf.Dataset
    This is an attempt to improve the performance of tensorflow in fpnn

--- src/fpnn/model_training.py
+++ src/fpnn/model_training.py
@@ -6,7 +6,7 @@ import pickle as pkl
 
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
-from tensorflow import keras, random
+from tensorflow import keras, random, data
 from keras import layers, models, utils
 
 # Set a random seed
@@ -47,21 +47,26 @@ class NeuralNet:
             assert len(activation_functions) == len(self.hidden_layers_layout), msg_layout_compatibility
             assert type(activation_functions) == tuple[str, ...]
             self.activation_functions = activation_functions
+            
+        # Attributes defined on execution
+        self._scaler: None | StandardScaler = None
+        self._scaled_X_train: np.ndarray = np.array([])
+        self._scaled_X_test: np.ndarray = np.array([])
 
     def __scale_features(self) -> None:
         # Define the scaler
         scaler = StandardScaler()
-        self.scaler = scaler.fit(self.X_train)
+        self._scaler = scaler.fit(self.X_train)
 
         # Scale train and test features
-        self.scaled_X_train = self.scaler.transform(self.X_train)
-        self.scaled_X_test = self.scaler.transform(self.X_test)
+        self._scaled_X_train = self._scaler.transform(self.X_train)
+        self._scaled_X_test = self._scaler.transform(self.X_test)
 
     def __build_model(self) -> models.Model:
         model = keras.Sequential()
         
-        # Add input layer to the NN model  
-        model.add(layers.Input(shape=(self.scaled_X_train.shape[1],)))
+        # Add input layer to the NN model 
+        model.add(layers.Input(shape=(self._scaled_X_train.shape[1],)))
         
         # Add hidden layers to the NN model
         for layer_units, activation_function in zip(self.hidden_layers_layout, self.activation_functions):
@@ -83,17 +88,37 @@ class NeuralNet:
 
         return model
 
+    def __convert_data_from_ndarray_to_dataset(
+        self, X, y, *, buffer_size: int = 1024
+    ) -> data.Dataset:
+        converted_X_y_as_dataset = data.Dataset.from_tensor_slices((X, y))
+        converted_X_y_as_dataset = converted_X_y_as_dataset.shuffle(buffer_size=1024)
+        converted_X_y_as_dataset = converted_X_y_as_dataset.batch(buffer_size)
+        converted_X_y_as_dataset = converted_X_y_as_dataset.prefetch(data.AUTOTUNE)
+        return converted_X_y_as_dataset
+    
     def __train_model(self) -> None:
         self.nn_model = self.__build_model()
 
         # Add an early stopping
         stop_early = keras.callbacks.EarlyStopping(monitor="val_mse", patience=500)
+        
+        # Buffer size compatible with most GPUs (if needed)
+        batch_size = 1024 if self._scaled_X_train.shape[0] > 1024 else self._scaled_X_train.shape[0]
+        
+        # Convert data from numpy arrays to tensorflow.data.Dataset
+        train_dataset = self.__convert_data_from_ndarray_to_dataset(
+            self._scaled_X_train, self.y_train, buffer_size=batch_size
+        )
+        validation_dataset = self.__convert_data_from_ndarray_to_dataset(
+            self._scaled_X_test, self.y_test, buffer_size=batch_size
+        )
+        
+        # Model training
         self.nn_model.fit(
-            self.scaled_X_train,
-            self.y_train,
-            batch_size=self.scaled_X_train.shape[0],
+            train_dataset,
             epochs=self.epochs,
-            validation_data=(self.scaled_X_test, self.y_test),
+            validation_data=validation_dataset,
             callbacks=[stop_early],
             verbose=0,
             workers=self.workers,
@@ -101,13 +126,13 @@ class NeuralNet:
         )
 
     def __model_metrics(self) -> None:
-        _, self.mae, self.mse = self.nn_model.evaluate(self.scaled_X_test, self.y_test)
+        _, self.mae, self.mse = self.nn_model.evaluate(self._scaled_X_test, self.y_test)
 
         print(f"MAE - {self.mae}\nRMSE - {np.sqrt(self.mse)}")
 
     def __plot_predicted_vs_experimental(self) -> None:
         # Get predicted values
-        y_pred = self.nn_model.predict(self.scaled_X_test)
+        y_pred = self.nn_model.predict(self._scaled_X_test)
 
         # Calculate R²
         r2 = r2_score(self.y_test, y_pred)
@@ -143,7 +168,7 @@ class NeuralNet:
         with open(
             save_model_path / f"scaler_{self.model_id}.pkl", "wb"
         ) as f:
-            pkl.dump(self.scaler, f)
+            pkl.dump(self._scaler, f)
 
         print("Model and scaler were succesfully saved")
 
